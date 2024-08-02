dd22b89489a1d3121a2e3889667ec92884ae9242
Eiji Tomonari! <eiji1@Eijis-MacBook-Pro.local>
2024-08-02 18:26:04 -0300

    Update main

--- src/main.jsx
+++ src/main.jsx
@@ -2,24 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
-// Check if the root element is inside a shadow root
 const rootElement = document.getElementById('zoggl-root');
 
-if (rootElement && rootElement.shadowRoot) {
-  // If using shadow root, use shadowRoot
-  ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    rootElement.shadowRoot.getElementById('zoggl-root')
-  );
-} else {
-  // Normal rendering
-  ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    rootElement
-  );
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  rootElement
+);
